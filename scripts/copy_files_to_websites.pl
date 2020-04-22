8471e8f1b240bca066fedd1328c5e0d194374ca7
Anne Lyle <ens-ap5@users.noreply.github.com>
2020-04-22 10:58:45 +0100

    Script mostly completed - needs testing and refining

--- scripts/copy_files_to_websites.pl
+++ scripts/copy_files_to_websites.pl
@@ -37,12 +37,12 @@ my ($SCRIPT_ROOT, $help, $version, $release, $site, $division, $home_only, $spec
 
 BEGIN{
   &GetOptions(
-              'help'          => \$help,
-              'release=s'     => \$release,
-              'site'          => \$site,
-              'division'      => \$division,
-              'home-only'     => \$home_only,
-              'species-only'  => \$species_only,
+              'help'            => \$help,
+              'release=s'       => \$release,
+              'site:s'          => \$site,
+              'division:s'      => \$division,
+              'home-only:s'     => \$home_only,
+              'species-only:s'  => \$species_only,
   );
 
   pod2usage(1) if $help;
@@ -53,42 +53,103 @@ BEGIN{
 my ($version, $eg_version) = split('-', $release);
 
 unless ($version && $eg_version) {
-  die "Please provide an Ensembl version and an NV release number, separated by a hyphen, e.g. '101-48'.\n";
+  die "Please provide an Ensembl version and an NV release number, separated by a hyphen, e.g. '--release=101-48'.\n";
 }
 
+## Default to all divisions
+my @divisions = $division ? ($division) : qw(bacteria fungi metazoa plants protists);
+
 ## Set destination - defaults to local checkout(s) in same directory as this repo
-my $OUTPUT_ROOT;
+my ($OUT_ROOT, $OUT_DIR, $division_string);
 
 if ($site) {
   if ($site !~ /staging|test|live/) {
     die "Please set the destination as either staging or live. Note that test sites use the live checkouts for the upcoming release.\n";
   }
   $site = 'live' if $site eq 'test';
-  $OUTPUT_ROOT = "/nfs/public/release/ensweb/$site/";
+  $OUT_ROOT = "/nfs/public/release/ensweb/$site";
+  $division_string = join('|', @divisions)."/www_$version";
 }
 else {
   if (!$division) {
     die "At the moment you can only make a local copy of content for one division at a time. Please specify a division on the command line.\n";
   }
-  ($OUTPUT_ROOT = $SCRIPT_ROOT) =~ s#ensembl-static/scripts##;
+  ($OUT_ROOT = $SCRIPT_ROOT) =~ s#/ensembl-static##;
+  $division_string = "eg-web-$division";
 }
 
-print "Copying files into $OUTPUT_ROOT";
+print "Copying files into $OUT_ROOT/$division_string...\n\n";
 
-## Parameters OK - make sure we're on the right branch
+## TODO - check that ensembl-static is on same branch as desired eg-version 
 
-my $checkout = "git checkout release/eg/$eg_version";
-system($checkout);
-my $pull = 'git pull';
-system($pull);
+## Define input directories for each type of content
+my $home_text_in  = '';
+my $home_img_in   = '/images';
+my $sp_text_in    = '/species';
+my $sp_img_in     = '/species/images';
 
-my @divisions = $division ? ($division) : qw(bacteria fungi metazoa plants protists);
+## Define output directories for each type of content
+my $home_text_out = 'htdocs/ssi';
+my $home_img_out  = 'htdocs/img';
+my $sp_text_out   = 'htdocs/ssi/species';
+my $sp_img_out    = 'htdocs/i/species';
+my ($input_dir, $output_dir);
 
 foreach my $div (@divisions) {
   print "Copying $div files...\n\n";
 
+  my $div_in_dir     = $SCRIPT_ROOT.'/'.$div;
+  my $div_out_dir    = $site ? sprintf('%s/www_%s', $OUT_ROOT, $version) : $OUT_ROOT;
+  $div_out_dir      .= "/eg-web-$div/";
 
+  ## Copy home content
+  unless ($species_only) {
+    ## Text files
+    $input_dir  = $div_in_dir.$home_text_in;
+    $output_dir = $div_out_dir.$home_text_out;
+    copy_files($input_dir, $output_dir);
 
+    ## Images
+    $input_dir  = $div_in_dir.$home_img_in;
+    $output_dir = $div_out_dir.$home_img_out;
+    copy_files($input_dir, $output_dir);
+
+    print "Copied home content\n";
+  }
+
+  ## Copy species content
+  unless ($home_only) {
+    ## Text files
+    $input_dir  = $div_in_dir.$sp_text_in;
+    $output_dir = $div_out_dir.$sp_text_out;
+    ## Note that we recurse into subdirectories for this one, as there are a lot of these files!
+    copy_files($input_dir, $output_dir, 1);
+
+    ## Images
+    $input_dir  = $div_in_dir.$sp_img_in;
+    $output_dir = $div_out_dir.$sp_img_out;
+    copy_files($input_dir, $output_dir);
+
+    print "Copied species content\n";
+  }
+
+}
+
+sub copy_files {
+  my ($in, $out, $recurse) = @_;
+
+  my $cmd = "cp $in/* $out";
+  print "Executing $cmd\n";
+  #system($cmd);
+
+  if ($recurse) {
+    $cmd = "cp $in/*/* $out";
+    print "Executing $cmd\n";
+    #system($cmd);
+    $cmd = "cp $in/*/*/* $out";
+    print "Executing $cmd\n";
+    #system($cmd);
+  }
 
 }
 
