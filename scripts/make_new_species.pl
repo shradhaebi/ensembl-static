8c8a95bd5c6da7957b4eccef655d815b08498f8c
Anne Lyle <annelyle@ebi.ac.uk>
2022-02-10 11:19:16 +0000

    Added/updated copyright notices where needed

--- scripts/make_new_species.pl
+++ scripts/make_new_species.pl
@@ -1,4 +1,23 @@
 #!/usr/bin/env perl
+# Copyright [2020-2022] EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use strict;
+use warnings;
+use Getopt::Std;
+use File::Copy;
+use FindBin '$Bin';
 
 # Creates new folder with markdown drafts for a species static content.
 # By default .md files are copied from templates, but other 
@@ -12,12 +31,6 @@
 # or when you want to copy from a previous species:
 #
 # perl ensembl-static/scripts/make_new_species.pl -n Camelina_sativa -c Beta_vulgaris
-
-use strict;
-use warnings;
-use Getopt::Std;
-use File::Copy;
-use FindBin '$Bin';
 
 my $TEMPLATEDIR = $Bin.'/../templates/';
 my @NVDIVISIONS = qw( plants fungi metazoa plants protists );
